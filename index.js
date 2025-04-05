f299cf6cd8322ffdac11cdf50f6cc0052cd01fbc
amir harrouche <34696387+orimouf@users.noreply.github.com>
2025-04-05 04:21:57 +0100

    final commit 1.1.0

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const request = require('request');
 const puppeteer = require('puppeteer'); 
 
 const pageUrl = "https://www.hermes.com/fr/fr/category/femme/sacs-et-petite-maroquinerie/sacs-et-pochettes/#|";//'https://www.google.com/';
 const hermesData = '';
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Home
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+});
+// Start the server
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'));
+});
 
 async function getDataFromSite(page) {
     console.log("page open ...");
